bb774cdedafc347178743e2970a0af4af24f01c3
josemehe <80551401+josemehe@users.noreply.github.com>
2022-09-12 00:46:44 -0500

    Minor changes
    I rearranged the code

--- ville.js
+++ ville.js
@@ -1,4 +1,6 @@
-'tile'
+'tile and basic items'
+  var keys = {UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39};
+  document.addEventListener("keydown", keyType);
   var c = document.getElementById("villeTile");
   var cContext = c.getContext("2d");
 'load tile'
@@ -61,6 +63,39 @@
     draw()
   }
 
+'pig move'
+    var x = 0, y = 0 'starting point'
+
+    function keyType(event)
+{
+    switch (event.keyCode)
+    {
+        case keys.UP:
+            console.log("UP funciona");
+            break;
+        
+        case keys.DOWN:
+            console.log("DOWN funciona");
+            break;
+        
+        case keys.LEFT:
+            console.log("LEFT funciona");
+            break;
+        
+        case keys.RIGHT:
+            console.log("RIGHT funciona");
+            break;
+
+        default:
+            console.log("otra tecla");
+            break;
+    }
+}
+
+function pigMove(x_i, y_i, x_f, y_f, canvas)
+{
+
+}
 function draw()
   {
     if (tile.loadOk)
